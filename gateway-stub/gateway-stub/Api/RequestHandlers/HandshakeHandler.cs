2875cd7ca6f6afee1a6d08857ed0c2368f3d83fe
DigIT <tabureddd@gmail.com>
2022-02-21 15:30:01 +0200

    update user data

--- gateway-stub/gateway-stub/Api/RequestHandlers/HandshakeHandler.cs
+++ gateway-stub/gateway-stub/Api/RequestHandlers/HandshakeHandler.cs
@@ -6,25 +6,29 @@ using GatewayStub.Api.Responses;
 using GatewayStub.Core;
 using GatewayStub.Core.Exchange;
 using GatewayStub.Core.WebSocket;
+using GatewayStub.Domain.Data;
 
 namespace GatewayStub.Api.RequestHandlers
 {
     public class HandshakeHandler : IRequestHandler
     {
         private readonly IWebSocketWrapper _socket;
+        private readonly IDataContext _dataContext;
 
         public byte Header => (byte) EHeader.Handshake;
 
-        public HandshakeHandler(IWebSocketWrapper socket)
+        public HandshakeHandler(IWebSocketWrapper socket, IDataContext dataContext)
         {
             _socket = socket;
+            _dataContext = dataContext;
         }
 
         public async Task Handle(IRequest request)
         {
             var handshakeRequest = (HandshakeRequest) request;
             Console.WriteLine($"Handshake received. Incoming data: AuthToken: {handshakeRequest.AuthToken}, ApiVersion: {handshakeRequest.ApiVersion}, SessionToken: {handshakeRequest.SessionToken}");
-            await _socket.Send(new HandshakeResponse(true, "TestUserId", string.Empty, null));
+            var userId = _dataContext.Matchmaking.UserId;
+            await _socket.Send(new HandshakeResponse(true, userId, string.Empty, null));
         }
     }
 }
